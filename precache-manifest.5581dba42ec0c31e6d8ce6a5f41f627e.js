4e4330741448dadf519478f6bc87f3ba0761fa83
Guillem Tubert <118167830+guillemtubert@users.noreply.github.com>
2023-11-15 13:32:44 +0100

    Updates

---
+++ precache-manifest.5581dba42ec0c31e6d8ce6a5f41f627e.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84e41fcf512077cc3fcae6a1e7955fc7",
+    "url": "/my-aphue/index.html"
+  },
+  {
+    "revision": "4baa7d3985952c30d925",
+    "url": "/my-aphue/static/css/2.69f70a5a.chunk.css"
+  },
+  {
+    "revision": "a8a56456977a5008b590",
+    "url": "/my-aphue/static/css/main.5976bef8.chunk.css"
+  },
+  {
+    "revision": "4baa7d3985952c30d925",
+    "url": "/my-aphue/static/js/2.2ae7dbe4.chunk.js"
+  },
+  {
+    "revision": "a8a56456977a5008b590",
+    "url": "/my-aphue/static/js/main.a41b9259.chunk.js"
+  },
+  {
+    "revision": "c86bd536dc5f6246d4bb",
+    "url": "/my-aphue/static/js/runtime~main.46ecb77a.js"
+  },
+  {
+    "revision": "9008302282fc8e1e7c7fd196b61d104b",
+    "url": "/my-aphue/static/media/1.90083022.jpg"
+  },
+  {
+    "revision": "3b18046c53ac126d1ee841114177144e",
+    "url": "/my-aphue/static/media/2.3b18046c.png"
+  },
+  {
+    "revision": "baa294f6ff1c88a307b3be33981cc8fc",
+    "url": "/my-aphue/static/media/2.baa294f6.jpg"
+  },
+  {
+    "revision": "0917889dd5ca03c27b3b9412557a9fdc",
+    "url": "/my-aphue/static/media/Flaticon.0917889d.svg"
+  },
+  {
+    "revision": "15cf01b77e18000ff8c30e5eb8132326",
+    "url": "/my-aphue/static/media/Flaticon.15cf01b7.woff"
+  },
+  {
+    "revision": "7ad910ab912a8296b41d09126628d1a4",
+    "url": "/my-aphue/static/media/Flaticon.7ad910ab.ttf"
+  },
+  {
+    "revision": "b9b714e8ebdc31809b71111250ee257e",
+    "url": "/my-aphue/static/media/Flaticon.b9b714e8.eot"
+  },
+  {
+    "revision": "3b75783f6f0c7cb5252911f1dae7cf28",
+    "url": "/my-aphue/static/media/IMG_20230807_145825.3b75783f.jpg"
+  },
+  {
+    "revision": "123b7d37cd8d85017b593ad45c328a13",
+    "url": "/my-aphue/static/media/close.123b7d37.svg"
+  },
+  {
+    "revision": "1dc35d25e61d819a9c357074014867ab",
+    "url": "/my-aphue/static/media/fontawesome-webfont.1dc35d25.ttf"
+  },
+  {
+    "revision": "25a32416abee198dd821b0b17a198a8f",
+    "url": "/my-aphue/static/media/fontawesome-webfont.25a32416.eot"
+  },
+  {
+    "revision": "c8ddf1e5e5bf3682bc7bebf30f394148",
+    "url": "/my-aphue/static/media/fontawesome-webfont.c8ddf1e5.woff"
+  },
+  {
+    "revision": "d7c639084f684d66a1bc66855d193ed8",
+    "url": "/my-aphue/static/media/fontawesome-webfont.d7c63908.svg"
+  },
+  {
+    "revision": "e6cf7c6ec7c2d6f670ae9d762604cb0b",
+    "url": "/my-aphue/static/media/fontawesome-webfont.e6cf7c6e.woff2"
+  },
+  {
+    "revision": "0d30576438a3f88c8ce7c0a23e0594ad",
+    "url": "/my-aphue/static/media/logo-sense-fons.0d305764.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/my-aphue/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/my-aphue/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/my-aphue/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/my-aphue/static/media/slick.f97e3bbf.svg"
+  }
+]);
